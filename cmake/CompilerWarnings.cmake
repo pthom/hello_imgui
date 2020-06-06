60184c9a3f543aa5deeb3a2253a9d94875c5b1aa
Pascal Thomet <pthomet@gmail.com>
2020-06-07 00:43:00 +0200

    add hello_imgui_demo_classic

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -55,7 +55,7 @@ function(set_project_warnings project_name)
       -Wconversion # warn on type conversions that may lose data
       -Wno-sign-conversion # warn on sign conversions
       -Wnull-dereference # warn if a null dereference is detected
-      -Wdouble-promotion # warn if float is implicit promoted to double
+#      -Wdouble-promotion # warn if float is implicit promoted to double
       -Wformat=2 # warn on security issues around functions that format output
                  # (ie printf)
   )

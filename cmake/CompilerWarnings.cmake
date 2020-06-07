2d922014c485e22435b78d6e31bfb531a4cf41ec
Pascal Thomet <pthomet@gmail.com>
2020-06-07 10:07:59 +0200

    mscv: disable -Werror

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -62,7 +62,7 @@ function(set_project_warnings project_name)
 
   if (WARNINGS_AS_ERRORS)
     set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
-    set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
+#     set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
   endif()
 
   set(GCC_WARNINGS

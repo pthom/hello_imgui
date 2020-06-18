ee2d2763fbe95c5ffc5dc4b3e9bb4d403f88c776
Pascal Thomet <pthomet@gmail.com>
2020-06-18 23:08:37 +0200

    ios cmake: rename hello_imgui_ios_set_dev_team

--- hello_imgui_cmake/ios/hello_imgui_ios.cmake
+++ hello_imgui_cmake/ios/hello_imgui_ios.cmake
@@ -1,32 +1,5 @@
-# * Bundle id
-#   set(MACOSX_BUNDLE_GUI_IDENTIFIER "com.mycompany.\${PRODUCT_NAME:identifier}")
-#   set(APP_TYPE MACOSX_BUNDLE)
-# Does *NOT* workk yet
-function(hello_imgui_ios_set_bundle_id app_name)
-#   set(MACOSX_BUNDLE_GUI_IDENTIFIER "com.mycompany.\${PRODUCT_NAME:identifier}" CACHE STRING "bundle id" FORCE)
-#   set(APP_TYPE MACOSX_BUNDLE)
-
-#    set_target_properties( ${app_name} PROPERTIES
-#        MACOSX_BUNDLE_GUI_IDENTIFIER "${IVS_BUNDLE_IDENTIFIER}"
-#        XCODE_ATTRIBUTE_DEVELOPMENT_TEAM "${IVS_DEVELOPMENT_TEAM}"
-#        XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "${IVS_CODE_SIGN_IDENTITY}"
-#        MACOSX_BUNDLE_BUNDLE_VERSION "${IVS_BUNDLE_BUILD}"
-#        MACOSX_BUNDLE_SHORT_VERSION_STRING "${IVS_BUNDLE_VERSION}"
-#        XCODE_ATTRIBUTE_TARGETED_DEVICE_FAMILY "2"
-#        XCODE_ATTRIBUTE_DEBUG_INFORMATION_FORMAT[variant=Debug] "dwarf"
-#        XCODE_ATTRIBUTE_DEBUG_INFORMATION_FORMAT[variant=RelWithDebInfo] "dwarf"
-#        XCODE_ATTRIBUTE_ENABLE_BITCODE NO
-#        XCODE_ATTRIBUTE_ASSETCATALOG_COMPILER_APPICON_NAME ${IVS_ICON_FILE}
-#        XCODE_ATTRIBUTE_ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME ${IVS_BUNDLE_SPLASH}
-#        XCODE_ATTRIBUTE_SKIP_INSTALL NO
-#        XCODE_ATTRIBUTE_INSTALL_PATH "$(LOCAL_APPS_DIR)"
-#    )
-
-    set(info_plist_dir_all ${HELLOIMGUI_BASEPATH}/hello_imgui_cmake/ios/info_plist)
-    set(info_plist ${info_plist_dir_all}/${plist_type}/Info.plist)
-
+function(hello_imgui_ios_set_dev_team app_name)
     set_target_properties( ${app_name} PROPERTIES
-        MACOSX_BUNDLE_GUI_IDENTIFIER "com.company.${app_name}"
         XCODE_ATTRIBUTE_DEVELOPMENT_TEAM ${CMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM}
         )
 endfunction()
@@ -90,7 +63,7 @@ endfunction()
 
 function(hello_imgui_ios_adapt app_name)
     hello_imgui_ios_check_development_team()
-    hello_imgui_ios_set_bundle_id(${app_name})
+    hello_imgui_ios_set_dev_team(${app_name})
     if (HELLOIMGUI_USE_SDL_OPENGL3)
         hello_imgui_ios_add_info_plist(${app_name} sdl)
     endif()

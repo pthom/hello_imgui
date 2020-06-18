a97dd3433ba653391b672971c30090ba66d0416f
Pascal Thomet <pthomet@gmail.com>
2020-06-18 23:26:34 +0200

    Add CI for iOS Simulator

--- tools/sdl_compile_ios.sh
+++ tools/sdl_compile_ios.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $THIS_DIR/external
+cd $THIS_DIR/../external || exit 1
 
 wget https://www.libsdl.org/release/SDL2-2.0.12.tar.gz
 tar xvfz SDL2-2.0.12.tar.gz
 rm SDL2-2.0.12.tar.gz
 ln -s SDL2-2.0.12 SDL
 cd SDL2-2.0.12/Xcode-iOS/SDL
-#xcodebuild  -project SDL.xcodeproj
-echo "For some reason, SDL build by command line (xcodebuild) fails."
-echo "Please build it SDL via XCode"
-echo "I will now open XCode : please click the menu Product, then Build."
-echo "You can quit XCode after this."
-read -p "Press enter to continue"
-open SDL.xcodeproj
+
+# Build for iphone
+xcodebuild -configuration Release -project SDL.xcodeproj -target libSDL-iOS
+
+# Build for simulator
+xcodebuild -configuration Release -project SDL.xcodeproj -target libSDL-iOS -arch x86_64 -sdk iphonesimulator

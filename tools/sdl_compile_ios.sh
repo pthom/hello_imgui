aa1fbd35a674aefe9039764b692e86b0d322e341
Pascal Thomet <pthomet@gmail.com>
2020-06-18 17:40:21 +0200

    Review sdl ios compile instructions (manual)

--- tools/sdl_compile_ios.sh
+++ tools/sdl_compile_ios.sh
@@ -8,5 +8,10 @@ tar xvfz SDL2-2.0.12.tar.gz
 rm SDL2-2.0.12.tar.gz
 ln -s SDL2-2.0.12 SDL
 cd SDL2-2.0.12/Xcode-iOS/SDL
-xcodebuild -target libSDL-iOS  -project SDL.xcodeproj
-
+#xcodebuild  -project SDL.xcodeproj
+echo "For some reason, SDL build by command line (xcodebuild) fails."
+echo "Please build it SDL via XCode"
+echo "I will now open XCode : please click the menu Product, then Build."
+echo "You can quit XCode after this."
+read -p "Press enter to continue"
+open SDL.xcodeproj

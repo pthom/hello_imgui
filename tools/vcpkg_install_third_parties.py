70809d283fc823544a0f7eb345b292d80a71261b
Pascal Thomet <pthomet@gmail.com>
2020-06-20 08:11:34 +0200

    vcpkg_install_third_parties: work on *nixes + windows

--- tools/vcpkg_install_third_parties.py
+++ tools/vcpkg_install_third_parties.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python
 
 import os
 import subprocess
@@ -7,15 +7,15 @@ THIS_DIR=os.path.dirname(os.path.realpath(__file__))
 os.chdir(THIS_DIR + "/..")
 
 if not os.path.isdir("vcpkg"):
-    subprocess.check_call("git clone https://github.com/Microsoft/vcpkg.git")
+    subprocess.check_call("git clone https://github.com/Microsoft/vcpkg.git", shell=True)
     os.chdir("vcpkg")
 
     if os.name == 'nt':
         subprocess.check_call("bootstrap-vcpkg.bat")
     else:
-        subprocess.check_call("bash ./bootstrap-vcpkg.sh")
+        subprocess.check_call("./bootstrap-vcpkg.sh", shell=True)
 
     if os.name == 'nt':
         subprocess.check_call("vcpkg.exe install glfw3 sdl2")
     else:
-        subprocess.check_call("./vcpkg install glfw3 sdl2")
+        subprocess.check_call("./vcpkg install glfw3 sdl2", shell=True)

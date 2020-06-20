b058e04f857fda58c33d6b244dd32f23fa626368
Pascal Thomet <pthomet@gmail.com>
2020-06-20 22:30:21 +0200

    fix vcpkg_install_sdl_android.sh

--- tools/android/vcpkg_install_sdl_android.sh
+++ tools/android/vcpkg_install_sdl_android.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $THIS_DIR/../..
 
-$THIS_DIR/_impl_vcpkg_create_android_triplets.sh
-cd $THIS_DIR/../../vcpkg
+git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
 ./bootstrap-vcpkg.sh
+$THIS_DIR/_impl_vcpkg_create_android_triplets.sh
 ./vcpkg install sdl2:arm-android sdl2:arm64-android

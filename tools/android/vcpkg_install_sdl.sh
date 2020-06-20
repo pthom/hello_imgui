0e16dd1fba131132948c2f626f3ed306b16fa9e3
Pascal Thomet <pthomet@gmail.com>
2020-06-20 22:15:24 +0200

    tools/android/vcpkg_install_sdl.sh: bootstrap

--- tools/android/vcpkg_install_sdl.sh
+++ tools/android/vcpkg_install_sdl.sh
@@ -4,4 +4,5 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 $THIS_DIR/_impl_vcpkg_create_android_triplets.sh
 cd $THIS_DIR/../../vcpkg
+./bootstrap-vcpkg.sh
 ./vcpkg install sdl2:arm-android sdl2:arm64-android

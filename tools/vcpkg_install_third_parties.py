5568649fd09ea1095852b78465083e6f0dca86bb
Pascal Thomet <pthomet@gmail.com>
2020-06-20 08:18:41 +0200

    vcpkg_install_third_parties: simplify

--- tools/vcpkg_install_third_parties.py
+++ tools/vcpkg_install_third_parties.py
@@ -6,16 +6,19 @@ import subprocess
 THIS_DIR=os.path.dirname(os.path.realpath(__file__))
 os.chdir(THIS_DIR + "/..")
 
+def run(cmd):
+    subprocess.check_call(cmd, shell=True)
+
 if not os.path.isdir("vcpkg"):
-    subprocess.check_call("git clone https://github.com/Microsoft/vcpkg.git", shell=True)
+    run("git clone https://github.com/Microsoft/vcpkg.git")
     os.chdir("vcpkg")
 
     if os.name == 'nt':
-        subprocess.check_call("bootstrap-vcpkg.bat")
+        run("bootstrap-vcpkg.bat")
     else:
-        subprocess.check_call("./bootstrap-vcpkg.sh", shell=True)
+        run("./bootstrap-vcpkg.sh")
 
     if os.name == 'nt':
-        subprocess.check_call("vcpkg.exe install glfw3 sdl2")
+        run("vcpkg.exe install glfw3 sdl2")
     else:
-        subprocess.check_call("./vcpkg install glfw3 sdl2", shell=True)
+        run("./vcpkg install glfw3 sdl2")

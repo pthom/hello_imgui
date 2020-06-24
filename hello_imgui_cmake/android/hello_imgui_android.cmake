e93ab96d97581f989c882f34b5b89ea564240d37
Pascal Thomet <pthomet@gmail.com>
2020-06-24 16:03:05 +0200

    call apkCMake_makeAndroidStudioProject

--- hello_imgui_cmake/android/hello_imgui_android.cmake
+++ hello_imgui_cmake/android/hello_imgui_android.cmake
@@ -1,7 +1,13 @@
 if (ANDROID)
 
+include(android/apkCMake/apkCMake)
+
 function (hello_imgui_platform_customization app_name)
-    message(WARNING "hello_imgui_add_app: hello_imgui_android_adapt is not implemented!")
+    set(apkCMake_applicationId ${HELLO_IMGUI_BUNDLE_IDENTIFIER})
+    set(apkCMake_applicationId org.libsdl.app) # Desperate move !!!
+    if (NOT HELLOIMGUI_IN_APKCMAKE_GENERATED_PROJECT)
+        apkCMake_makeAndroidStudioProject(${app_name})
+    endif()
 endfunction()
 
 endif(ANDROID)

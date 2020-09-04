a21d54a7998819ff4ce65c1ba2672c446aaf23e3
Pascal Thomet <pthomet@gmail.com>
2020-09-04 22:06:37 +0200

    Add emscripten deploy scripts

---
+++ tools/emscripten/deploy/emscripten_build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO_DIR=$THIS_DIR/../../..
+cd $REPO_DIR
+
+if [ ! -d build_emscripten ]; then
+  mkdir build_emscripten
+fi
+
+cd build_emscripten
+source ~/emsdk/emsdk_env.sh
+emcmake cmake ..
+make -j 4
---
+++ tools/emscripten/deploy/emscripten_deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This script is for authors only (it deploys the emscripten demos onto the server)
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO_DIR=$THIS_DIR/../../..
+cd $REPO_DIR
+
+./tools/emscripten/deploy/emscripten_build.sh
+cd build_emscripten
+rsync -vaz src/* pascal@traineq.org:HTML/HelloImGui/

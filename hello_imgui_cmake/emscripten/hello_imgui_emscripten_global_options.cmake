1ad63e8ea1aa52ad3a5acd58b2734661017b4da0
Pascal Thomet <pthomet@gmail.com>
2023-01-22 12:07:35 +0100

    emscripten build options: -g4 became -gsource-map

--- hello_imgui_cmake/emscripten/hello_imgui_emscripten_global_options.cmake
+++ hello_imgui_cmake/emscripten/hello_imgui_emscripten_global_options.cmake
@@ -4,11 +4,10 @@
 
 set(BUILD_SHARED_LIBS OFF)
 
-# Debug build is  -O0 -g4 in order to have the call stack + source map
+# Debug build is  -O0 -gsource-map in order to have the call stack + source map
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g4 -s DISABLE_EXCEPTION_CATCHING=0")
-
+    set(CMAKE_CXX_FLAGS_DEBUG "-O0 -gsource-map")
     # source-map-base enables to be able to "kind of debug" from the js developper console
     # Advice: always debug the same exact source code on desktop before even trying to run in the browser
-    set(link_options_debug -O0 --source-map-base http://localhost:8000/src/)
+    # --source-map-base http://localhost:8000/src/
 endif()

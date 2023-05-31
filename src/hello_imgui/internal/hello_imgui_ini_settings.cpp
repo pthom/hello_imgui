b4dc1f576fe37b850732fd82c7764670c72546d9
Pascal Thomet <pthomet@gmail.com>
2023-05-31 19:12:44 +0200

    Settings: add SplitInitParts / JoinIniParts (with tests)

--- src/hello_imgui/internal/hello_imgui_ini_settings.cpp
+++ src/hello_imgui/internal/hello_imgui_ini_settings.cpp
@@ -1,10 +1,99 @@
 #include "hello_imgui/internal/hello_imgui_ini_settings.h"
 #include "hello_imgui/internal/inicpp.h"
+#include "hello_imgui/internal/fplus_all_in_one.hpp"
+#include "imgui_internal.h"
+
 
 namespace HelloImGui
 {
     namespace HelloImGuiIniSettings
     {
+        // Test if a line looks like an IniPart intro, e.g.:
+        //      ;;;<<<imgui>>>;;;
+        bool _IsIniPartName(const std::string& line)
+        {
+            return (line.find(";;;<<<")) == 0 && (line.rfind(">>>;;;") == line.size() - 6);
+        }
+
+        // Reads an IniPart intro name, e.g.:
+        //      ;;;<<<imgui>>>;;;
+        // =>
+        //      imgui
+        std::string _ReadIniPartName(const std::string& line)
+        {
+            IM_ASSERT(_IsIniPartName(line));
+            IM_ASSERT(line.size() > 12);
+            std::string name = line.substr(6, line.size() - 12);
+            return name;
+        }
+
+        IniParts SplitIniParts(const std::string& s)
+        {
+            IniParts iniParts;
+            std::optional<IniParts::IniPart> currentPart;
+
+            auto lines = fplus::split_lines(true, s);
+            for (const std::string& line: lines)
+            {
+                if (_IsIniPartName(line))
+                {
+                    if (currentPart.has_value())
+                        iniParts.Parts.push_back(*currentPart);
+
+                    currentPart = IniParts::IniPart();
+                    currentPart->Name = _ReadIniPartName(line);
+                }
+                else
+                    currentPart->Content += line + "\n";
+            }
+
+            if (currentPart.has_value())
+                iniParts.Parts.push_back(*currentPart);
+
+            return iniParts;
+        }
+
+        std::string JoinIniParts(const IniParts& iniParts)
+        {
+            std::string r;
+            for (const auto& iniPart: iniParts.Parts)
+            {
+                r += ";;;<<<" + iniPart.Name + ">>>;;;\n";
+                r += iniPart.Content;
+            }
+            return r;
+        }
+
+        bool  IniParts::HasIniPart(const std::string& name)
+        {
+            bool r = false;
+            for(const auto& iniPart: Parts)
+                if (iniPart.Name == name)
+                    return true;
+            return false;
+        }
+        std::string IniParts::GetIniPart(const std::string& name)
+        {
+            IM_ASSERT(HasIniPart(name));
+            for(const auto& iniPart: Parts)
+                if (iniPart.Name == name)
+                    return iniPart.Content;
+            return "ERROR call HasIniPart before GetIniPart!!!!";
+        }
+        void IniParts::SetIniPart(const std::string& name, const std::string& content)
+        {
+            if (HasIniPart(name))
+            {
+                for(auto& iniPart: Parts)
+                    if (iniPart.Name == name)
+                        iniPart.Content = content;
+
+            }
+            else
+                Parts.push_back(IniPart{name, content});
+        }
+
+
         void WriteLastRunWindowBounds(const std::string& iniFilename, const ScreenBounds& windowBounds)
         {
             ini::IniFile iniFile;
--- src/hello_imgui/internal/hello_imgui_ini_settings.h
+++ src/hello_imgui/internal/hello_imgui_ini_settings.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "hello_imgui/screen_bounds.h"
+#include "hello_imgui/docking_params.h"
 
 #include <string>
 #include <optional>
@@ -10,7 +11,52 @@ namespace HelloImGui
 {
     namespace HelloImGuiIniSettings
     {
+        /*
+         HelloImGui stores its settings in a file that groups several ini file, and looks like this
+
+            ;;;<<<imgui>>>;;;
+            [Window][Main window (title bar invisible)]
+            Pos=0,0
+            Size=1000,800
+            Collapsed=0
+
+            [Window][Debug##Default]
+            Pos=60,60
+            Size=400,400
+            Collapsed=0
+
+            [Docking][Data]
+
+            ;;;<<<appWindow>>>;;;
+            [WIN]
+            WindowPosition=393,238
+            WindowSize=971,691
+
+            ;;;<<<otherIniInfo>>>;;;
+            ...
+         */
+
+        struct IniParts
+        {
+            struct IniPart
+            {
+                std::string Name;
+                std::string Content;
+            };
+
+            bool           HasIniPart(const std::string& name);
+            std::string    GetIniPart(const std::string& name);
+            void           SetIniPart(const std::string& name, const std::string& content);
+
+            std::vector<IniPart> Parts;
+        };
+        IniParts SplitIniParts(const std::string& s);
+        std::string JoinIniParts(const IniParts& parts);
+
         void WriteLastRunWindowBounds(const std::string& iniFilename, const ScreenBounds& windowBounds);
         std::optional<ScreenBounds> ReadLastRunWindowBounds(const std::string& iniFilename);
+
+        void WriteDockableWindowsVisibility(const std::string& iniFilename, const DockingParams& dockingParams);
+        void ReadDockableWindowsVisibility(const std::string& iniFilename, DockingParams* inOutDockingParams);
     }
 }

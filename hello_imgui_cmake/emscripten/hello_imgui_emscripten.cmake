c0426b8f64e7235d0827115a37724f4d22a97a42
Pascal Thomet <pthomet@gmail.com>
2023-10-20 12:05:08 +0200

    hello_imgui_emscripten_set_shell_file: use custom shell file unmodified if provided

--- hello_imgui_cmake/emscripten/hello_imgui_emscripten.cmake
+++ hello_imgui_cmake/emscripten/hello_imgui_emscripten.cmake
@@ -5,24 +5,25 @@ include(${CMAKE_CURRENT_LIST_DIR}/hello_imgui_emscripten_global_options.cmake)
 function(hello_imgui_emscripten_set_shell_file app_name)
 
     if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/shell.emscripten.html)
-        set(shell_template_file ${CMAKE_CURRENT_SOURCE_DIR}/shell.emscripten.html)
+        set(shell_file ${CMAKE_CURRENT_SOURCE_DIR}/shell.emscripten.html)
+        message(WARNING "app_name=${app_name} shell_file=${shell_file}")
     else()
         set(shell_template_file ${HELLOIMGUI_BASEPATH}/hello_imgui_cmake/emscripten/shell.emscripten.html)
+        get_property(runtime_output_directory TARGET ${app_name} PROPERTY RUNTIME_OUTPUT_DIRECTORY)
+        if ("${runtime_output_directory}" STREQUAL "")
+            set(real_output_directory ${CMAKE_CURRENT_BINARY_DIR})
+        else()
+            set(real_output_directory ${runtime_output_directory})
+        endif()
+
+        set(shell_file ${real_output_directory}/shell.emscripten.in.html)
+        configure_file(
+            ${shell_template_file}
+            ${shell_file}
+            @ONLY
+        )
     endif()
 
-    get_property(runtime_output_directory TARGET ${app_name} PROPERTY RUNTIME_OUTPUT_DIRECTORY)
-    if ("${runtime_output_directory}" STREQUAL "")
-        set(real_output_directory ${CMAKE_CURRENT_BINARY_DIR})
-    else()
-        set(real_output_directory ${runtime_output_directory})
-    endif()
-
-    set(shell_file ${real_output_directory}/shell.emscripten.in.html)
-    configure_file(
-        ${shell_template_file}
-        ${shell_file}
-        @ONLY
-    )
     target_link_options(
         ${app_name} 
         PRIVATE

819850f8a3b9c40a09a29a4028d0d16ca52ad498
Pascal Thomet <pthomet@gmail.com>
2023-12-01 23:58:28 +0100

    hello_imgui_add_app.cmake: layout

--- hello_imgui_cmake/hello_imgui_add_app.cmake
+++ hello_imgui_cmake/hello_imgui_add_app.cmake
@@ -90,8 +90,9 @@ include(${CMAKE_CURRENT_LIST_DIR}/apple/hello_imgui_apple.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/android/hello_imgui_android.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/emscripten/hello_imgui_emscripten.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/windows/hello_imgui_windows.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/assets/hello_imgui_assets.cmake)
 
+
+include(${CMAKE_CURRENT_LIST_DIR}/assets/hello_imgui_assets.cmake)
 
 
 set(apkCMake_projectTemplateFolder ${CMAKE_CURRENT_LIST_DIR}/android/apkCMake/templates/sdl)

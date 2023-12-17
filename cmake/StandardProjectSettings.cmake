79cbda8109e15c2e5a866b57f5aefde0562dfd26
Pascal Thomet <pthomet@gmail.com>
2023-12-17 16:40:07 +0100

    cmake: quiet message / ccache

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -14,8 +14,6 @@ find_program(CCACHE ccache)
 if(CCACHE)
   message("using ccache")
   set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
-else()
-  message("ccache not found cannot use")
 endif()
 
 # Generate compile_commands.json to make it easier to work with clang based

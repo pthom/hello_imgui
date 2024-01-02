6bbc55f5fe5a77ab9f8bf73df4cb495404533a0e
Pascal Thomet <pthomet@gmail.com>
2024-01-02 23:10:56 +0100

    fix MingW on linux

--- cmake/mingw_i686.cmake
+++ cmake/mingw_i686.cmake
@@ -1,13 +1,22 @@
 # run "i686-w64-mingw32-g++ -print-sysroot" to get the sysroot path
-execute_process(COMMAND i686-w64-mingw32-g++ -print-sysroot OUTPUT_VARIABLE MINGW_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-# Check the sysroot path
-if(NOT MINGW_SYSROOT)
+execute_process(COMMAND
+    i686-w64-mingw32-g++ -print-sysroot
+    OUTPUT_VARIABLE MINGW_SYSROOT
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    RESULT_VARIABLE MINGW_SYSROOT_RESULT
+)
+if(NOT MINGW_SYSROOT_RESULT EQUAL 0)
     message(FATAL_ERROR "
         x86_64-w64-mingw32-g++ -print-sysroot failed!
         Did you install mingw, and is it in your PATH?
     ")
 endif()
+
+# if running on linux host, x86_64-w64-mingw32-g++ -print-sysroot may return an empty string
+if(${CMAKE_SYSTEM_NAME} MATCHES "Linux" AND NOT MINGW_SYSROOT)
+    set(MINGW_SYSROOT /usr/i686-w64-mingw32 )
+endif()
+message(STATUS "MINGW_SYSROOT: ${MINGW_SYSROOT}")
 
 
 # Define the compilers to use for C and C++

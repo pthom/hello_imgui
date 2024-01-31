dfc2af43d0ced829bf5dce35b993d2c9a7f87415
Pascal Thomet <pthomet@gmail.com>
2024-01-31 20:38:30 +0100

    Add test_vcpkg_himgui

---
+++ hello_imgui_cmake/overlay_vcpkg/test_vcpkg_himgui/test.cpp
@@ -0,0 +1,29 @@
+#include "hello_imgui/hello_imgui.h"
+#include "imgui.h"
+
+int main()
+{
+    HelloImGui::RunnerParams runnerParams;
+    runnerParams.backendType = HelloImGui::BackendType::Glfw; // Sdl fails with Vulkan
+    runnerParams.callbacks.ShowGui = []() {
+        #ifdef HELLOIMGUI_HAS_OPENGL
+        ImGui::Text("HELLOIMGUI_HAS_OPENGL");
+        #endif
+        #ifdef HELLOIMGUI_HAS_METAL
+        ImGui::Text("HELLOIMGUI_HAS_METAL");
+        #endif
+        #ifdef HELLOIMGUI_HAS_VULKAN
+        ImGui::Text("HELLOIMGUI_HAS_VULKAN");
+        #endif
+        #ifdef HELLOIMGUI_HAS_DIRECTX11
+        ImGui::Text("HELLOIMGUI_HAS_DIRECTX11");
+        #endif
+        #ifdef HELLOIMGUI_HAS_DIRECTX12
+        ImGui::Text("HELLOIMGUI_HAS_DIRECTX12");
+        #endif
+
+        ImGui::ShowDemoWindow();
+
+    };
+    HelloImGui::Run(runnerParams);
+}

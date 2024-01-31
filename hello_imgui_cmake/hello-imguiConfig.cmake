aeaf91080e9b67f48e452a2c764434585ed13cbf
Pascal Thomet <pthomet@gmail.com>
2024-01-31 14:58:34 +0100

    typo hello-imgui-targets.cmake

--- hello_imgui_cmake/hello-imguiConfig.cmake
+++ hello_imgui_cmake/hello-imguiConfig.cmake
@@ -1,3 +1,3 @@
 include(CMakeFindDependencyMacro)
 # find_dependency(xxx 2.0)
-include(${CMAKE_CURRENT_LIST_DIR}/hello_imguiTargets.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/hello-imgui-targets.cmake)

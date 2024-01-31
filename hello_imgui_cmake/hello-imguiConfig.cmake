328f2140331c01cf642405854bd15727f3d8b6db
Pascal Thomet <pthomet@gmail.com>
2024-01-31 15:14:14 +0100

    cmake config: add hello_imgui_add_app.cmake

--- hello_imgui_cmake/hello-imguiConfig.cmake
+++ hello_imgui_cmake/hello-imguiConfig.cmake
@@ -1,4 +1,6 @@
 include(CMakeFindDependencyMacro)
-# find_dependency(xxx 2.0)
-include(${CMAKE_CURRENT_LIST_DIR}/hello-imgui-targets.cmake)
 find_dependency(imgui CONFIG REQUIRED)
+
+include(${CMAKE_CURRENT_LIST_DIR}/../hello_imgui_cmake/hello_imgui_add_app.cmake)
+
+include(${CMAKE_CURRENT_LIST_DIR}/hello-imgui-targets.cmake)

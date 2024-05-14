05a2472886538eff41e21a38808fdfbbfa64369f
Pascal Thomet <pthomet@gmail.com>
2024-05-14 14:45:51 +0200

    fix doc

--- src/hello_imgui/hello_imgui_widgets.h
+++ src/hello_imgui/hello_imgui_widgets.h
@@ -64,7 +64,7 @@ namespace HelloImGui
     //       # Somewhere in the application state
     //       text_input = hello_imgui.InputTextData("My text", multiline=True, size_em=ImVec2(10, 3))
     //       # In the GUI function
-    //       changed = hello_imgui.InputTextResizable("Label", text_input)
+    //       changed = hello_imgui.input_text_resizable("Label", text_input)
     //       ```
     struct InputTextData
     {

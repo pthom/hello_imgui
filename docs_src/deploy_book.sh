5f3c675bd9a61340c191011794c22c9c0587af93
Pascal Thomet <pthomet@gmail.com>
2025-07-19 11:31:28 +0200

    _generate_book_impl: assume jupyter-book is in path

--- docs_src/deploy_book.sh
+++ docs_src/deploy_book.sh
@@ -65,12 +65,12 @@ echo "=================================================="
 # Run REPO_DIR/tools/doc/process_md_docs.py
 # using python from the virtual environment venv
 # Check python presence in the venv/ directory
-if [[ ! -f "$REPO_DIR/venv/bin/python" ]]; then
-    echo "- deploy_book: ERROR: ******** Please create the virtual environment first ********"
-    exit 1
-fi
-# Activate the virtual environment
-source "$REPO_DIR/venv/bin/activate"
+#if [[ ! -f "$REPO_DIR/venv/bin/python" ]]; then
+#    echo "- deploy_book: ERROR: ******** Please create the virtual environment first ********"
+#    exit 1
+#fi
+## Activate the virtual environment
+#source "$REPO_DIR/venv/bin/activate"
 # Run the script
 python "$REPO_DIR/tools/doc/process_md_docs.py"
 if [[ $? -ne 0 ]]; then

b24c11cb8cf3e741423db419e1296f521a6c5369
Pascal Thomet <pthomet@gmail.com>
2026-03-16 23:47:10 +0100

    fix hello_imgui_include_opengl.h for ios

--- src/hello_imgui/hello_imgui_include_opengl.h
+++ src/hello_imgui/hello_imgui_include_opengl.h
@@ -2,9 +2,15 @@
 #ifdef HELLOIMGUI_HAS_OPENGL
 
 #if defined(HELLOIMGUI_USE_GLES3)
-    #if defined(IOS)
-        #include <OpenGLES/ES3/gl.h>
-        #include <OpenGLES/ES3/glext.h>
+    #if defined(__APPLE__)
+        #include <TargetConditionals.h>
+        #if TARGET_OS_IOS || TARGET_OS_SIMULATOR
+            #include <OpenGLES/ES3/gl.h>
+            #include <OpenGLES/ES3/glext.h>
+        #else
+            #include <GLES3/gl3.h>
+            #include <GLES3/gl3ext.h>
+        #endif
     #elif defined(__EMSCRIPTEN__)
         #include <GLES3/gl3.h>
         //#include <GLES3/gl2ext.h>
@@ -13,9 +19,15 @@
         #include <GLES3/gl3ext.h>
     #endif
 #elif defined(HELLOIMGUI_USE_GLES2)
-    #ifdef IOS
-        #include <OpenGLES/ES2/gl.h>
-        #include <OpenGLES/ES2/glext.h>
+    #if defined(__APPLE__)
+        #include <TargetConditionals.h>
+        #if TARGET_OS_IOS || TARGET_OS_SIMULATOR
+            #include <OpenGLES/ES2/gl.h>
+            #include <OpenGLES/ES2/glext.h>
+        #else
+            #include <GLES2/gl2.h>
+            #include <GLES2/gl2ext.h>
+        #endif
     #else
         #include <GLES2/gl2.h>
         #include <GLES2/gl2ext.h>
